d2f4806bf7e54075398b77468780676ccea5809a
youngentry <gentry_@naver.com>
2022-08-11 17:50:39 +0900

    220811

--- js/main.js
+++ js/main.js
@@ -5,4 +5,8 @@ $(function () {
     centerMode: true,
     centerPadding: "10%",
   });
+
+  // $('.jarSlider').on('afterChange',function(e,s,c) {
+  //   $('.jarSlider.slick-current')
+  // })
 });
