ba6ee3587dfde411bad87fa214a5b11036ac4156
youngentry <90388461+youngentry@users.noreply.github.com>
2022-08-17 00:07:52 +0900

    220817

--- js/main.js
+++ js/main.js
@@ -14,4 +14,12 @@ $(function () {
   $(".reviewSlider").slick({
     slidesToShow: 3,
   });
+
+  $(".Re2 .right").mousemove(function (e) {
+    var x = e.pageX;
+    var y = e.pageY;
+    $(".Re2ScrollPointer").css("left", x + "px");
+    $(".Re2ScrollPointer").css("top", y + "px");
+    $(".Re2ScrollPointer").css("display", "block");
+  });
 });
