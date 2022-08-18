0ea02ab994826a8e5b8be6921b07f9b1274e17ef
youngentry <gentry_@naver.com>
2022-08-18 12:27:36 +0900

    220818

--- js/main.js
+++ js/main.js
@@ -25,11 +25,11 @@ $(function () {
     slidesToShow: 3,
   });
 
-  $(".Re2 .right").mousemove(function (e) {
+  $(".Review .right").mousemove(function (e) {
     var x = e.pageX;
     var y = e.pageY;
-    $(".Re2ScrollPointer").css("left", x + "px");
-    $(".Re2ScrollPointer").css("top", y + "px");
-    $(".Re2ScrollPointer").css("display", "block");
+    $(".ReviewScrollPointer").css("left", x + "px");
+    $(".ReviewScrollPointer").css("top", y + "px");
+    $(".ReviewScrollPointer").css("display", "block");
   });
 });
